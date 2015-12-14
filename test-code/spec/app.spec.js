1db62244c7a3be0a21a8e6156f5cb38931bef88b
elimorolli <elimorolli@gmail.com>
2015-12-14 12:37:04 +0100

    Ho modificato il file del test

--- test-code/spec/app.spec.js
+++ test-code/spec/app.spec.js
@@ -1,7 +1,24 @@
-describe("App", function() {
+describe("Coach", function() {
     
-  it("should..", function() { 
-      pending();
+  it("Penso che torni true", function() { 
+      expect(Coach.start("normal")).toBe(true);
   });  
+    
+    it("Penso che torni false",function(){
+        expect(Coach.start("hard")).toBe(false);
+    });
+    
+    it("Penso venga 2000",function(){
+        Coach.start("normal");
+       Coach.track("bike",5); 
+        expect(Coach.track("run",10)).toBe(2000);
+        
+    });
+    it("Dovrebbe trnare 2, ma siccome viene moltiplicato per 100, torna 200",function(){
+        Coach.start("normal");
+       Coach.track("bike",5); 
+    Coach.track("run",10);
+    expect(Coach.checkProgress()).toBe(200);
+    });
  
 });
